88096ce12d7e731d9654d60b127a59280ebb29ea
unknown <PeaceCreation2022@gmail.com>
2023-06-15 08:45:48 -0700

    The WEbsite is Updated

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Start Jquery Plaguins
+$('.product').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 3000,
+  });
+
+  // Truck Slider 
+  $('.service').slick({
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
+
+// End Jquery Plaguins
